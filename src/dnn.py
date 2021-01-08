a2d94ff6e53e25ce4a643740f3a3c58f37ce04ac
KristianWold <kriswold@student.matnat.uio.no>
2021-01-08 19:59:38 +0100

    Add multithread qiskit

--- src/dnn.py
+++ src/dnn.py
@@ -1,50 +1,7 @@
 import numpy as np
 import qiskit as qk
-import torch
-import torch.nn as nn
 from copy import deepcopy
-
-class GD():
-    def __init__(self, lr=0.01):
-        self.lr = lr
-
-    def initialize(self, dims):
-        pass
-
-    def __call__(self, weight_gradient_list):
-        return weight_gradient_list
-
-
-class Adam():
-    def __init__(self, lr=0.01, beta1=0.9, beta2=0.999, eps=1e-8):
-        self.lr = lr
-        self.beta1 = beta1
-        self.beta2 = beta2
-        self.eps = eps
-
-        self.m = []
-        self.v = []
-        self.t = 0
-
-    def initialize(self, dims):
-        for dim in dims:
-            self.m.append(np.zeros(dim))
-            self.v.append(np.zeros(dim))
-
-    def __call__(self, weight_gradient_list):
-        self.t += 1
-        weight_gradient_modified = []
-
-        for grad, m_, v_ in zip(weight_gradient_list, self.m, self.v):
-            m_ = self.beta1*m_ + (1 - self.beta1)*grad
-            v_ = self.beta2*v_ + (1 - self.beta2)*grad**2
-
-            m_hat = m_/(1 - self.beta1**self.t)
-            v_hat = v_/(1 - self.beta2**self.t)
-            grad_modified = m_hat/(np.sqrt(v_hat) + self.eps)
-            weight_gradient_modified.append(grad_modified)
-
-        return weight_gradient_modified
+from .optimizers import Adam, GD
 
 
 class Ansatz():
@@ -74,6 +31,60 @@ class Encoder():
             circuit.cx(storage[i], storage[i + 1])
 
         return circuit
+
+
+class Sigmoid():
+
+    def __call__(self, x):
+        x = 1/(1 + np.exp(-x))
+
+        return x
+
+    def derivative(self, x):
+        x = x*(1 - x)
+
+        return x
+
+
+class Identity():
+
+    def __call__(self, x):
+
+        return x
+
+    def derivative(self, x):
+
+        return 1
+
+
+class Dense():
+    def __init__(self, n_inputs=None, n_outputs=None, scale=1, activation = None):
+
+        self.n_inputs = n_inputs
+        self.n_outputs = n_outputs
+        self.scale = scale
+        self.activation = activation
+
+        self.weight = np.random.normal(0, 1, (n_inputs+1, n_outputs))
+
+    def __call__(self, inputs):
+        x = inputs@self.weight[:-1] + self.weight[-1].reshape(1,-1)
+        x = self.activation(x)
+
+        return self.scale*x
+
+    def grad(self, inputs, delta):
+        output = self(inputs).T
+        delta = self.activation.derivative(output)*delta
+
+        weight_gradient = (delta@inputs).T
+        bias_gradient = delta.T
+
+        weight_gradient = np.concatenate((weight_gradient, bias_gradient), axis=0)
+
+        delta = self.weight[:-1]@delta
+
+        return weight_gradient, delta
 
 
 class QLayer():
@@ -139,110 +150,8 @@ class QLayer():
 
         return weight_gradient, delta
 
-"""
-class CLayer(nn.Module):
-    def __init__(self, n_inputs=None, n_outputs=None, scale = 1, activation = None):
-        super().__init__()
 
-        self.n_inputs = n_inputs
-        self.n_outputs = n_outputs
-        self.scale = scale
-        self.activation = activation
-
-        self.weight = np.normal(0, 1, (n_inputs, n_outputs+1))
-
-    def __call__(self, inputs):
-        input = deepcopy(inputs).reshape(1,-1)
-
-        if not type(input) is np.ndarray:
-            input = input.detach()
-        self.input = torch.tensor(input, requires_grad=True, dtype=torch.float)
-        self.output = self.layer(self.input).reshape(1,-1)
-        self.output = self.activation(self.output)
-        self.zero_grad()
-        return deepcopy(self.output.detach().numpy())
-
-    def grad(self, inputs, delta):
-        self(inputs)
-        input_partial = torch.zeros((self.output.shape[1], self.input.shape[1]), requires_grad = False)
-        weight_partial = torch.zeros((self.layer.weight.shape[0], self.layer.weight.shape[1] + 1), requires_grad=False)
-        for j in range(self.output.shape[1]):
-            self.output[0, j].backward(retain_graph = True)
-            input_partial[j, :] = self.input.grad[0, :]
-            weight_partial[j,:-1] = self.layer.weight.grad[j,:]
-            weight_partial[j,-1] = self.layer.bias.grad[j]
-
-        self.zero_grad()
-        weight_partial = weight_partial.detach().numpy().T
-        input_partial = input_partial.detach().numpy().T
-
-        weight_gradient = inputs*delta
-        bias_gradient = delta
-        weight_gradient = np.concatenate((weight_gradient, bias_gradient), axis=1)
-
-        delta = input_partial*(self.layer.weight.detach().numpy().T@delta)
-
-        return weight_gradient, delta
-"""
-
-
-class CLayer(nn.Module):
-    def __init__(self, n_inputs=None, n_outputs=None, activation = None):
-        super().__init__()
-
-        self.n_inputs = n_inputs
-        self.n_outputs = n_outputs
-        self.layer = nn.Linear(n_inputs, n_outputs)
-        self.activation = activation
-
-    @property
-    def weight(self):
-        weight_ = torch.cat((self.layer.weight, self.layer.bias.reshape(-1,1)), 1)
-        return(weight_.T)
-
-    def update_weight(self,lr_grad):
-        with torch.no_grad():
-            self.layer.weight += lr_grad.T[:,:-1]
-            self.layer.bias += lr_grad.T[:,-1]
-        self.zero_grad()
-
-    def __call__(self, inputs):
-        input = deepcopy(inputs).reshape(1,-1)
-        self.zero_grad()
-        if not type(input) is np.ndarray:
-            input = input.detach()
-        self.input = torch.tensor(input, requires_grad=True, dtype=torch.float)
-        self.output = self.layer(self.input).reshape(1,-1)
-        self.output = self.activation(self.output)
-
-        return deepcopy(self.output.detach().numpy())
-
-    def grad(self, inputs, delta):
-        self(inputs)
-        input_partial = torch.zeros((self.output.shape[1], self.input.shape[1]), requires_grad = False)
-        weight_partial = torch.zeros((self.layer.weight.shape[0], self.layer.weight.shape[1] + 1), requires_grad=False)
-        for j in range(self.output.shape[1]):
-            self.output[0, j].backward(retain_graph = True)
-            input_partial[j, :] = self.input.grad[0, :]
-            weight_partial[j,:-1] = self.layer.weight.grad[j,:]
-            weight_partial[j,-1] = self.layer.bias.grad[j]
-
-        self.zero_grad()
-        weight_partial = weight_partial.detach().numpy().T
-        input_partial = input_partial.detach().numpy().T
-
-        #weight_gradient = inputs*delta
-        #bias_gradient = delta
-        #weight_gradient = np.concatenate((weight_gradient, bias_gradient), axis=1)
-
-        #delta = input_partial@delta
-        weight_gradient = weight_partial*delta.T
-        delta = input_partial@delta
-
-        return weight_gradient, delta
-
-
-class QNN():
+class NeuralNetwork():
     def __init__(self, layers, optimizer):
         self.layers = layers
         self.dim = []
@@ -252,7 +161,7 @@ class QNN():
             self.dim.append(layer.weight.shape)
 
         self.optimizer.initialize(self.dim)
-        self.a = 0
+        self.a = []
         self.weight_gradient_list = []
 
     def __call__(self, x):
@@ -262,6 +171,10 @@ class QNN():
             x = layer(x)
             self.a.append(x)
 
+    def predict(self, x):
+        self(x)
+        return self.a[-1]
+
     def backward(self, X, Y):
         n = X.shape[0]
         weight_gradient_list = []
@@ -270,6 +183,9 @@ class QNN():
             weight_gradient_list.append(weight_gradient)
 
         for x, y in zip(X,Y):
+            #x = x.reshape(1,-1)
+            #y = y.reshape(1,-1)
+
             self(x)
             y_pred = self.a[-1]
             delta = (y_pred - y).reshape(-1,1)
@@ -282,30 +198,27 @@ class QNN():
 
     def step(self):
         weight_gradient_modified = self.optimizer(self.weight_gradient_list)
+
         for layer, grad in zip(self.layers, weight_gradient_modified):
-                if not type(layer.weight) is np.ndarray:
-                    grad_ = torch.tensor(grad.copy(), requires_grad = False)
-                    layer.update_weight(torch.nn.Parameter(-self.optimizer.lr*grad_))
-                else:
-                    layer.weight += -self.optimizer.lr*grad
+            layer.weight += -self.optimizer.lr*grad
 
     def set_shots(self, shots):
         for layer in self.layers:
             layer.shots = shots
 
 
-def sequential(dim, backend, shots=1000):
+def sequential(dim, backend, reps=1, shots=1000):
     layers = []
     for i in range(len(dim)-2):
         in_dim = dim[i]
         out_dim = dim[i+1]
-        layer = QLayer(n_qubits=in_dim, n_inputs=in_dim, n_outputs=out_dim, encoder=Encoder(), ansatz=Ansatz(), reps=1, scale=np.pi, backend=backend, shots=shots)
+        layer = QLayer(n_qubits=in_dim, n_inputs=in_dim, n_outputs=out_dim, encoder=Encoder(), ansatz=Ansatz(), reps=reps, scale=np.pi, backend=backend, shots=shots)
         layers.append(layer)
 
-    layer = QLayer(n_qubits=dim[-2], n_inputs=dim[-2], n_outputs=dim[-1], encoder=Encoder(), ansatz=Ansatz(), reps=1, scale=1, backend=backend, shots=shots)
+    layer = QLayer(n_qubits=dim[-2], n_inputs=dim[-2], n_outputs=dim[-1], encoder=Encoder(), ansatz=Ansatz(), reps=reps, scale=1, backend=backend, shots=shots)
     layers.append(layer)
 
     optimizer = Adam(lr=0.01)
-    network = QNN(layers, optimizer)
+    network = NeuralNetwork(layers, optimizer)
 
     return network
