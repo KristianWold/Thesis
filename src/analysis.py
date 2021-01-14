96dc556f07dad73ba327b8fc94da30b963e31825
KristianWold <kriswold@student.matnat.uio.no>
2021-01-14 20:26:02 +0100

    Implement FIM

--- src/analysis.py
+++ src/analysis.py
@@ -2,17 +2,26 @@ import numpy as np
 import qiskit as qk
 from copy import deepcopy
 from neuralnetwork import *
+from tqdm.notebook import tqdm
 
 
 def fisher_information_matrix(network, x, y):
+    # extract weights from network
+    weight_list = []
+    for layer in network.layers:
+        weight = layer.weight.flatten()
+        weight_list.append(weight)
+
+    weight_list = np.concatenate(weight_list).reshape(-1, 1)
+
     fim_list = []
-    for x_, y_ in tqdm(zip(x, y)):
+    for x_, y_ in tqdm(zip(x, y), total=len(x)):
         x_ = x_.reshape(1, -1)
         y_ = y_.reshape(1, -1)
 
+        # extract gradient from network
         network.backward(x_, y_)
         weight_gradient_list = []
-
         for grad in network.weight_gradient_list:
             weight_gradient_list.append(grad.flatten())
 
@@ -25,4 +34,6 @@ def fisher_information_matrix(network, x, y):
 
     fim = np.mean(np.array(fim_list), axis=0)
 
-    return fim
+    fr = weight_list.T @ fim @ weight_list
+
+    return fim, fr
